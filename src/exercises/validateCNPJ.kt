e10f0c53c4ff9ed52bfa68dc19b19c0af2008dcf
Camila Grandini <camila.grandini@outlook.com>
2019-09-01 23:51:35 -0300

    CNPJ validate and mask

--- src/exercises/validateCNPJ.kt
+++ src/exercises/validateCNPJ.kt
@@ -2,10 +2,42 @@ package exercises
 
 /*
 1. Validate CNPJ Format (Mask)
+2. Validate CNPJ Digits
  */
 
-fun main() {
+fun isCNPJ(document: String): Boolean {
+    val numbers = arrayListOf<Int>()
 
+    document.filter { it.isDigit() }.forEach {
+        numbers.add(it.toString().toInt())
+    }
+
+    if (numbers.size != 16) return false
+
+    //repeticao
+    (0..9).forEach { n ->
+        val digits = arrayListOf<Int>()
+        (0..14).forEach { digits.add(n) }
+        if (numbers == digits) return false
+    }
+
+    //digito 1
+    val dv1  =  11 - (arrayOf(5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2).mapIndexed { index, i ->
+        i * numbers[index]
+    }).sum().rem(11)
+    numbers.add(dv1)
+
+    //digito 2
+    val dv2  =  11 - (arrayOf(6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2).mapIndexed { index, i ->
+        i * numbers[index]
+    }).sum().rem(11)
+
+
+
+    return numbers[12] == dv1 && numbers[13] == dv2
+}
+
+fun main() {
     val expected = "([0-9]{2})([0-9]{3})([0-9]{3})([0-9]{4}([0-9]{2}))".toRegex()
 
     //format = XX.XXX.XXX/YYYY-ZZ
@@ -13,6 +45,8 @@ fun main() {
 
     print("Enter a CNPJ: ")
     val formattedCNPJ = readLine()!!.replace(expected, expectedFormat)
-    print("Your formatted CNPJ is: $formattedCNPJ")
 
+    if (isCNPJ(formattedCNPJ))
+        println("CNPJ $formattedCNPJ is valid!")
+    else println("Invalid CNPJ!")
 }
